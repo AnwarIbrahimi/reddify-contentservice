550a80c3e1d10645884968d80d0fddebb0dd76eb
Anwar <465821@student.fontys.nl>
2023-12-19 15:26:34 +0100

    added new database

--- ContentService/Migrations/20231219142538_InitialCreate.Designer.cs
+++ ContentService/Migrations/20231219142538_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContentService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20231123104739_InitialCreate")]
+    [Migration("20231219142538_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
